7967e8724998d8b7c823b52404e1595509eabd16
Hartman- <thehartman8@gmail.com>
2016-07-31 21:57:34 -0400

    Nuke shot directory management, rough

--- basket/nuke/assetManager.py
+++ basket/nuke/assetManager.py
@@ -5,11 +5,75 @@
 
 # set the project to the local directory
 
-import sys, os
+import sys, os, re
 import nuke
 
 from PySide.QtCore import *
 from PySide.QtGui import *
+
+class Environment:
+    def __init__(self):
+
+        # DEFINE EXAMPLE ENVIRONMENT
+        os.environ['SHOW'] = 'PROJ_local'
+        os.environ['SEQ'] = 'xyz'
+        os.environ['SHOT'] = '010'
+
+    def rootDir(self, user):
+        return 'C:\\Users\\' + str(user) + '\\Desktop\\LAW\\'
+
+    def nukeDir(self):
+        curDir = os.path.join(self.rootDir('IanHartman'), os.getenv('SHOW'), 'Working', os.getenv('SEQ'), os.getenv('SHOT'), '07. Comp')
+        if not os.path.isdir( curDir ):
+            raise ValueError, 'NUKE Directory does not exist'
+        return curDir
+
+    # SET SHOW ENV VARIABLE
+    def setShow(self, show):
+        os.environ['SHOW'] = str(show)
+
+    # SET SEQ ENV VARIABLE
+    def setSeq(self, seq):
+        os.environ['SEQ'] = str(seq)
+
+    # SET SHOT ENV VARIABLE
+    def setShot(self, shot):
+        os.environ['SHOT'] = str(shot)
+
+
+class HManager:
+    def __init__(self):
+        self.env = Environment()
+        self.nkDir = self.env.nukeDir()
+
+    def easySave(self):
+        description = nuke.getInput( 'Script Variable', 'bashComp' ).replace(' ','')
+
+        fileSaved = False
+        version = 1
+        while not fileSaved:
+            nkName = '%s_%s_%s_%s_v%02d.nk' % ( os.getenv('SHOW'), os.getenv('SEQ'), os.getenv('SHOT'), description, version)
+            nkPath = os.path.join(self.nkDir, nkName)
+            if os.path.isfile(nkPath):
+                version += 1
+                continue
+            nuke.scriptSaveAs(nkPath)
+            fileSaved = True
+        return nkPath
+
+    # CHECK FOR VERSION IN SCRIPT NAME
+    def checkScriptName(self):
+        if not re.search(r'[vV]\d+', nuke.root().name()):
+            raise NameError, 'Please include a version number and save script again.'
+
+    def refreshUI(self):
+        nuke.updateUI()
+
+
+class HGui:
+    def __init__(self):
+
+
 
 class LocalizeFiles:
     def __init__(self):
