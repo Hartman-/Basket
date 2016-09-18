697d897da3e58256c01deb31519b482ec142248c
Hartman <imh29@drexel.edu>
2016-09-18 16:14:15 -0400

    attempted windows batch file for launcher

---
+++ basket/windows_nuke.bat
@@ -0,0 +1,4 @@
+SET PATH=%PATH%;C:\Python27\ArcGIS10.4
+SET PYTHONPATH=%PYTHONPATH%;C:\Python27\ArcGIS10.4;X:\Classof2017\LobstersAreWeird\basket
+SET NUKE_PATH=X:\Classof2017\LobstersAreWeird\basket\nuke
+python %1 %2
