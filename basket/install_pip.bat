aa7e934be8d5620c28092a074c1a2bc4dbdc9ef1
Hartman <imh29@drexel.edu>
2016-09-26 10:39:32 -0400

    added ways use pip

--- basket/install_pip.bat
+++ basket/install_pip.bat
@@ -1,3 +1,3 @@
 @SET PATH=%PATH%;C:\Python27\ArcGIS10.4;C:\Python27\ArcGIS10.4\scripts;C:\Python27\ArcGIS10.4\Lib;
 @SET PYTHONPATH=%PYTHONPATH%;C:\Python27\ArcGIS10.4;C:\Python27\ArcGIS10.4\scripts;
-@pip install PySide
+@PySide -version
