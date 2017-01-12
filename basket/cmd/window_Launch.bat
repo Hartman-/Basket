25701c1288c7261c5c08baa4f90d4a56c69494f4
Hartman <imh29@drexel.edu>
2017-01-12 15:18:04 -0500

    updated launcher script

--- basket/cmd/window_Launch.bat
+++ basket/cmd/window_Launch.bat
@@ -1,8 +1,8 @@
-@SET PATH=%PATH%;C:\Python27\ArcGIS10.4;C:\Python27\ArcGIS10.4\Lib\site-packages;X:\Classof2017\LobstersAreWeird\basket;
-@SET PYTHONPATH=%PYTHONPATH%;C:\Python27\ArcGIS10.4;X:\Classof2017\LobstersAreWeird\basket;X:\Classof2017\LobstersAreWeird\basket\maya;
-@SET NUKE_PATH=X:\Classof2017\LobstersAreWeird\basket\nuke
-@set RMS_SCRIPT_PATHS=%RMS_SCRIPT_PATHS%;X:\Classof2017\LobstersAreWeird\basket\renderman
+@SET DIGMPATH=\\awexpress.westphal.drexel.edu\digm_share\Classof2017
+@SET PATH=%PATH%;C:\Python27\ArcGIS10.4;C:\Python27\ArcGIS10.4\Lib\site-packages;%DIGMPATH%\LobstersAreWeird\basket;
+@SET PYTHONPATH=%PYTHONPATH%;C:\Python27\ArcGIS10.4;%DIGMPATH%\LobstersAreWeird\basket;%DIGMPATH%\LobstersAreWeird\basket\maya;
+@SET NUKE_PATH=%DIGMPATH%\LobstersAreWeird\basket\nuke
+@set RMS_SCRIPT_PATHS=%RMS_SCRIPT_PATHS%;%DIGMPATH%\LobstersAreWeird\basket\renderman
 @set RMSPROJ=\\awexpress.westphal.drexel.edu\digm_anfx\SRPJ_LAW
 @SET MAYA_APP_DIR=C:\Users\%USERNAME%\Documents\maya
-@CD /d X:\Classof2017\LobstersAreWeird\basket
-@python BasketLauncher.py
+@python %DIGMPATH%\LobstersAreWeird\basket\BasketLauncher.py
